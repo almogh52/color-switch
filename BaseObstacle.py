dd0de8a6efbb522290206d0f2baa9d0460cffbc1
Almog Hamdani <almogh52@gmail.com>
2018-10-30 17:39:04 +0200

    Move all rotations to be done on the Obstacle instead of the BaseObstacle class

--- BaseObstacle.py
+++ BaseObstacle.py
@@ -9,32 +9,13 @@ class BaseObstacle():
         # Set default properties
         self.height = 0
         self.width = 0
-        self.rotateAmount = 0
-        self.image = None
-        self.tk_image = None
-        self.pil_image = None
 
         # Bind to the onload event, to init the pos of the obstacle
         self.parent.bind("<Map>", self.init_pos, add="+")
 
-        self.parent.bind("<Map>", self.rotate, add="+")
-
     def init_pos(self, event):       
         # Place the obstacle(image) in the middle of the screen in the initial y pos
         self.image = self.parent.create_image(self.parent.winfo_width() / 2, -self.height, image=self.tk_image)
-
-    def rotate(self, event=None):
-        # Set the tkinter image as the original rotated with the current rotate amount
-        self.tk_image = ImageTk.PhotoImage(self.pil_image.rotate(self.rotateAmount))
-
-        # Set the tkinter image as the obstacle image
-        self.parent.itemconfig(self.image, image=self.tk_image)
-
-        # Increase rotate amount (Need to do modulo 360 in order to remove unnecessary full circle rotations)
-        self.rotateAmount += 2
-        self.rotateAmount %= 360
-
-        self.parent.after(10, self.rotate)
 
     def move_obstacle(self, delta):
         # Place the obstacle in the new pos
--- Obstacle.py
+++ Obstacle.py
@@ -10,9 +10,29 @@ class Obstacle(BaseObstacle):
         self.width = 200
         self.height = 200
 
+        # Set rotate as 0
+        self.rotateAmount = 0
+
         # Open the obstacle image
         self.pil_image = Image.open(os.path.dirname(os.path.abspath(__file__)) + r"/Resources/Obstacle.png")
         self.pil_image = self.pil_image.resize((200, 200), Image.BICUBIC)
 
+        # Create a tkinter image from the pil image
         self.tk_image = ImageTk.PhotoImage(self.pil_image)
+
+        self.parent.bind("<Map>", self.rotate, add="+")
+
+    
+    def rotate(self, event=None):
+        # Set the tkinter image as the original rotated with the current rotate amount
+        self.tk_image = ImageTk.PhotoImage(self.pil_image.rotate(self.rotateAmount))
+
+        # Set the tkinter image as the obstacle image
+        self.parent.itemconfig(self.image, image=self.tk_image)
+
+        # Increase rotate amount (Need to do modulo 360 in order to remove unnecessary full circle rotations)
+        self.rotateAmount += 2
+        self.rotateAmount %= 360
+
+        self.parent.after(10, self.rotate)
     
