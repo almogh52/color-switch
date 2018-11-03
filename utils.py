e63a833945faf6f0ef488b1176b7b6c880289648
Almog Hamdani <almogh52@gmail.com>
2018-11-03 15:42:12 +0200

    Add a function that calculates the collision point of two surfaces

--- utils.py
+++ utils.py
@@ -10,3 +10,22 @@ def load_and_resize_image(path, size):
 
     # Convert it to a pygame image from the image bytes, size and mode
     return pygame.image.fromstring(image.tobytes(), image.size, image.mode)
+
+def get_overlap_point(surface1, rect1, surface2, rect2):
+    # Get the masks of both surfaces
+    mask1 = pygame.mask.from_surface(surface1)
+    mask2 = pygame.mask.from_surface(surface2)
+
+    # Set the offset between the 2 masks as the position differences
+    offset = (rect2.x - rect1.x, rect2.y - rect1.y)
+
+    # Get the relative overlap point from first surface if the two surfaces overlap each other
+    relative_overlap_point = mask1.overlap(mask2, offset)
+
+    # If the overlap point exists, calculate the real overlap point in the screen using the position of the first surface
+    if relative_overlap_point is not None:
+        real_overlap_point = (relative_overlap_point[0] + rect1.x - 2, relative_overlap_point[1] + rect1.y - 2)
+    else:
+        real_overlap_point = None
+
+    return real_overlap_point
