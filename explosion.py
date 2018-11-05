3ef8e5066fc476a81c07d86c1a0659aeb25af4cb
Almog Hamdani <almogh52@gmail.com>
2018-11-05 16:02:04 +0200

    Add random velocity for each axis in the ball explosion + Change random colors + Add missing super constructor

--- explosion.py
+++ explosion.py
@@ -4,27 +4,27 @@ import utils
 
 class BallExplosion(pygame.sprite.Sprite):
     BALL_RADIUS = 3
-    VELOCITY = 15
     BALL_COLORS = [
-        (26, 188, 156),
-        (241, 196, 15),
+        (0, 255, 204),
+        (255, 101, 0),
         (211, 84, 0),
-        (231, 76, 60),
-        (46, 204, 113),
-        (52, 152, 219),
-        (142, 68, 173),
-        (52, 73, 94),
-        (57, 32, 160)
+        (255, 23, 0),
+        (0, 255, 108),
+        (0, 152, 255),
+        (179, 0, 255)
     ]
 
     def __init__(self, screen):
+        # Call the super constructor
+        pygame.sprite.Sprite.__init__(self)
+
         self.screen = screen
 
         # Get the screen's rect to be used in the random position
         screen_rect = screen.get_rect()
 
         # Set a random y for the ball
-        self.y = random.randint(50, screen_rect.height) - 200
+        self.y = random.randint(0, screen_rect.height)
 
         # Get a random bool that indicates if the ball should start from left or right
         if utils.random_bool():
@@ -43,10 +43,13 @@ class BallExplosion(pygame.sprite.Sprite):
         # Get a random color for the ball
         self.color = random.choice(self.BALL_COLORS)
 
+        # Generate a random velocity for the x and y axis
+        self.velocity = (random.randint(2, 8), random.randint(2, 8))
+
     def update(self):
         # Change the ball's position by the velocity
-        self.x += self.VELOCITY * self.direction[0]
-        self.y += self.VELOCITY * self.direction[1]
+        self.x += self.velocity[0] * self.direction[0]
+        self.y += self.velocity[1] * self.direction[1]
 
         # Draw the circle onto the screen
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.BALL_RADIUS)
